4c818816e07310e96785989a85b65e3dbda553f6
gouravgupta7799 <118657865+gouravgupta7799@users.noreply.github.com>
2023-01-13 16:27:26 +0530

    Add files via upload

--- project4.js
+++ project4.js
@@ -2,8 +2,16 @@ let Name = document.getElementById('name');
 let email = document.getElementById('email');
 let form = document.getElementById('addForm');
 form.addEventListener('submit', additems);
+obj = {
+  personName: '',
+  personEmail: ''
+}
 
 function additems(e) {
   e.preventDefault();
-  localStorage.setItem(Name.value, email.value)
+  obj.personName = Name.value;
+  obj.personEmail = email.value;
+  let newobj = JSON.stringify(obj);
+  localStorage.setItem(newobj, newobj);
+  console.log(newobj)
 }
